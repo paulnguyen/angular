316b9baa9ffec07f2ea3e020663207724fc2d7fc
paulnguyen <paul.nguyen@sjsu.edu>
2021-08-11 22:45:03 -0700

    update

--- reddit/src/app/app.component.ts
+++ reddit/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reddit';
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false; 
+  }
+
+
 }
