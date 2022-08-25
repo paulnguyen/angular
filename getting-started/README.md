55e49ccbfd55a117f51ebdf97d5a5652b16662f3
paulnguyen <paul.nguyen@sjsu.edu>
2022-08-24 21:42:14 -0700

    update

--- getting-started/README.md
+++
@@ -1,3 +0,0 @@
-# Angular Getting Started
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pnguyen-angular-getting-started)
--- testing/src/testing/jasmine-matchers.d.ts
+++
@@ -1,6 +0,0 @@
-// tslint:disable-next-line: no-namespace
-declare namespace jasmine {
-  interface Matchers<T> {
-    toHaveText(actual: any, expectationFailOutput?: any): jasmine.CustomMatcher;
-  }
-}
